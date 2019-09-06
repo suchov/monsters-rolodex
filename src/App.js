73651fe63689d2c67f486a09b1673fffd208acc6
Artem Sychov <artem.suchov@gmail.com>
2019-09-06 14:35:13 +0300

    change the passing function to an error funtion of the class

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
@@ -27,7 +31,7 @@ class App extends Component {
       <div className="App">
         <SearchBox
           placeholder="search monters"
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
