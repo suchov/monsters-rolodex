1595b6dc76adb124df4159cf2a2152f980692e17
Artem Sychov <artem.suchov@gmail.com>
2019-09-06 14:24:45 +0300

    move search to a search component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list";
-
+import { SearchBox } from "./components/search-box/search-box";
 class App extends Component {
   constructor() {
     super();
@@ -25,10 +25,9 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="search monsters"
-          onChange={e => this.setState({ searchField: e.target.value })}
+        <SearchBox
+          placeholder="search monters"
+          handleChange={e => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
