488349fae60530c9a1faca0b8b52abfa69817de0
Artem Sychov <artem.suchov@gmail.com>
2019-09-07 21:25:41 +0300

    add some fonts and styling

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1> Monster Rolodex</h1>
         <SearchBox
           placeholder="search monters"
           handleChange={this.handleChange}
