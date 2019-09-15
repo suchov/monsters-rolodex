8412107258898007d4f4723bc8a238c9412469b3
Artem Sychov <artem.suchov@gmail.com>
2019-09-15 21:23:59 +0300

    Updates

---
+++ precache-manifest.0c1fe2b38b726632229dfd48192a3f9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf4928ae551a91bfba960e83aa651cc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cd0937125735d8906556",
+    "url": "/monsters-rolodex/static/css/main.dad204d6.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monsters-rolodex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "cd0937125735d8906556",
+    "url": "/monsters-rolodex/static/js/main.f77a42e1.chunk.js"
+  },
+  {
+    "revision": "c7b84e328d40a43989ce",
+    "url": "/monsters-rolodex/static/js/runtime~main.45a8f4a3.js"
+  }
+]);
