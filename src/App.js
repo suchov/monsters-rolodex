9845403e0651f3f65b8589dbfb9ff4845ca8da88
Artem Sychov <artem.suchov@gmail.com>
2020-02-11 21:04:46 +0200

    add a state to class component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: "Hello My My"
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          Hi from Artem
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({ string: "Hello Artem" })}>
+            Change Text
+          </button>
         </header>
       </div>
     );
