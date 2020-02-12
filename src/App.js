7ac142a88630dd009503feb73324d2cabbc16d63
Artem Sychov <artem.sychov@exabeam.com>
2020-02-12 18:38:01 +0200

    fetch the users from the typicode

--- src/App.js
+++ src/App.js
@@ -5,21 +5,14 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asc2'
-        },
-        {
-          name: 'Zombie',
-          id: 'asc3'
-        }
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(user => this.setState({monster: user}))
   }
   render() {
     return (
