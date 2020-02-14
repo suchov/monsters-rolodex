d798f75fb88d554f4adb8a3ccdf2ef4c42d01daa
Artem Sychov <artem.suchov@gmail.com>
2020-02-14 16:15:47 +0200

    add the search field  input, it's state and updating state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
@@ -18,6 +19,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
